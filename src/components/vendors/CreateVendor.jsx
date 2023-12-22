4479fbf050f770d89a052cf0b842ff7d1301fb7d
Hiram Fernandes <hiram.sd@gmail.com>
2023-12-22 15:35:38 -0300

    Added style to create vendor Added vercel config file

--- src/components/vendors/CreateVendor.jsx
+++ src/components/vendors/CreateVendor.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react"
 import { ToastContainer, toast }  from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
+import './CreateVendor.css';
+
 const CreateVendor = () => {
     const [name, setName] = useState();
     const [location, setLocation] = useState();
@@ -63,7 +65,7 @@ const CreateVendor = () => {
     return (
         <div className="main">
             <ToastContainer />
-            <div className="create-vendor__main-header">
+            <div className="createExpense__placeholder">
                 <h1> Create Vendor </h1>
                 <form onSubmit={submitHandler} className='create-vendor__form'>
                     <div className='create-vendor__item'>
